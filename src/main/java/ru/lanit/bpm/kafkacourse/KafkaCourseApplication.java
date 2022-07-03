ebd2d493f9e98ab93f8848e8f9348edef3c3b303
VOsipenkov <VOsipenkov@lanit.ru>
2022-07-03 19:28:03 +0300

    init

--- src/main/java/ru/lanit/bpm/kafkacourse/KafkaCourseApplication.java
+++ src/main/java/ru/lanit/bpm/kafkacourse/KafkaCourseApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KafkaCourseApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaCourseApplication.class, args);
 	}
-
 }
